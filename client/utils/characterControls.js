6d6f2ad3b3920f92863e6ff8f341cfe56fc910d2
Santiago Suarez <santiagosuarez86@gmail.com>
2022-01-14 14:34:44 -0300

    emprolijar

--- client/utils/characterControls.js
+++ client/utils/characterControls.js
@@ -6,23 +6,19 @@ export class CharacterControls {
   animationsMap = new Map(); // Walk, Run, Idle
   orbitControl;
   camera;
-
   // state
   toggleRun = true;
   currentAction;
-
   // temporary data
   walkDirection = new THREE.Vector3();
   rotateAngle = new THREE.Vector3(0, 1, 0);
   rotateQuarternion = new THREE.Quaternion();
   cameraTarget = new THREE.Vector3();
-
   // constants
   fadeDuration = 0.2;
   runVelocity = 10;
   walkVelocity = 2;
   rollVelocity = 12;
-
   constructor(
     jugador,
     mixer,
@@ -44,32 +40,25 @@ export class CharacterControls {
     this.camera = camera;
     this.updateCameraTarget(0, 0);
   }
-
   switchRunToggle() {
     this.toggleRun = !this.toggleRun;
   }
-
   switchAtacar() {
     // console.log("atacar");
     this.atacar = 1;
   }
-
   switchAtacarStop() {
     // console.log("stop atacar");
     this.atacar = 0;
   }
-
   switchRoll() {
     // console.log("switchRoll");
     this.roll = 1;
-  }
-
-  
+  }  
   switchRollStop() {
     // console.log("switchRoll");
     this.roll = 0;
   }
-
   switchJuntar() {
     // console.log("juntar");
     this.juntar = 1;
@@ -78,11 +67,8 @@ export class CharacterControls {
     // console.log("juntar");
     this.juntar = 0;
   }
-
-
   update(delta, keysPressed) {
     const directionPressed = DIRECTIONS.some((key) => keysPressed[key] == true);  
-
     var play = "";
     if (directionPressed && this.toggleRun) {
       play = "Run";
@@ -100,19 +86,14 @@ export class CharacterControls {
     if (directionPressed && this.roll == 1) {
       play = "Roll";
     }
-
     if (this.currentAction != play) {
       const toPlay = this.animationsMap.get(play);
       const current = this.animationsMap.get(this.currentAction);
-
       current.fadeOut(this.fadeDuration);
       toPlay.reset().fadeIn(this.fadeDuration).play();
-
       this.currentAction = play;
     }
-
     this.mixer.update(delta);
-
     if (this.currentAction == "Run" || this.currentAction == "Walk" || this.currentAction == "Roll") {
       // calculate towards camera direction
       var angleYCameraDirection = Math.atan2(
@@ -121,14 +102,12 @@ export class CharacterControls {
       );
       // diagonal movement angle offset
       var directionOffset = this.directionOffset(keysPressed);
-
       // rotate jugador
       this.rotateQuarternion.setFromAxisAngle(
         this.rotateAngle,
         angleYCameraDirection + directionOffset
       );
       this.jugador.quaternion.rotateTowards(this.rotateQuarternion, 0.2);
-
       // calculate direction
       this.camera.getWorldDirection(this.walkDirection);
       this.walkDirection.y = 0;
@@ -138,12 +117,9 @@ export class CharacterControls {
       // run/walk velocity
       var velocity =
         this.currentAction == "Run" ? this.runVelocity : this.walkVelocity;
-
         if(this.currentAction == "Roll" ) {
           velocity = this.rollVelocity;
-
         }
-
       // move jugador & camera
       const moveX = -this.walkDirection.x * velocity * delta;
       const moveZ = -this.walkDirection.z * velocity * delta;
@@ -152,22 +128,18 @@ export class CharacterControls {
       this.updateCameraTarget(moveX, moveZ);
     }
   }
-
   updateCameraTarget(moveX, moveZ) {
     // move camera
     this.camera.position.x += moveX;
     this.camera.position.z += moveZ;
-
     // update camera target
     this.cameraTarget.x = this.jugador.position.x;
     this.cameraTarget.y = this.jugador.position.y + 1;
     this.cameraTarget.z = this.jugador.position.z;
     this.orbitControl.target = this.cameraTarget;
   }
-
   directionOffset(keysPressed) {
     var directionOffset = 0; // w
-
     if (keysPressed[S]) {
       if (keysPressed[D]) {
         directionOffset = Math.PI / 4; // w+a
@@ -187,7 +159,6 @@ export class CharacterControls {
     } else if (keysPressed[A]) {
       directionOffset = -Math.PI / 2; // d
     }
-
     return directionOffset;
   }
 }
